bf8b07ced1a78b39491f716e87f1c09d8d3fddf5
134912 <1349129242@qq.com>
2021-08-31 14:54:20 +0800

    push tools

--- aapd/PMI.py
+++ aapd/PMI.py
@@ -3,27 +3,36 @@ import numpy as np
 da = pd.read_excel(r"C:\Users\哈哈\PycharmProjects\datasets\aapd\metrix.csv",index_col=0)#共现矩阵文件的路径
 tag = pd.read_csv(r"C:\Users\哈哈\PycharmProjects\datasets\aapd\datacsv.csv")#tag的csv文件路径
 list = da.values.tolist()
+list1 = da.values.tolist()
 
-def newPMI(tag,list):
+def newPMI(tag,list,list1):
     l = len(tag)
     for i in range(len(list)):
-        conx = list[i][i]
-        px = conx/l
+
+        conx = list1[i][i]
+        px = conx / l
+        #print(conx)
         for j in range(len(list)):
-            cony = list[j][j]
+            cony = list1[j][j]
             py = cony/l
-            con = list[i][j]
+            con = list1[i][j]
             pxy = con/l
+
             #print(con)
+            #if i >= j:
+
             if con != 0:
                 pmi = (pxy / (px * py))
                 list[i][j] = pmi
             else:
                 list[i][j] = 0
+    #print(list1)
+            #else:
+                #list[i][j]=list[j][i]
     test = pd.DataFrame(data=list)
-    test.to_csv(r"C:\Users\哈哈\PycharmProjects\datasets\aapd\newpmicsv1.csv")  # 保存计算好的PMI
+    test.to_csv(r"C:\Users\哈哈\PycharmProjects\datasets\aapd\pmi1.csv")  # 保存计算好的PMI
 
 
-c = newPMI(tag,list)
+c = newPMI(tag,list,list1)
 
 
