dcc8d8a4610347bd89020332d543ad906140619d
134912 <1349129242@qq.com>
2021-09-02 10:59:36 +0800

    push tools

--- tools/spilttag.py
+++ tools/spilttag.py
@@ -3,12 +3,15 @@ from pandas import DataFrame
 
 
 # data=pd.read_csv('shop.csv',sep='\t',header=0)
-data = pd.read_csv('../aapd/datacsv.csv',sep=',',index_col=0)
-data1 = data.sample(frac=0.7,replace=False)#flase是无放回的切割，有放回的切
+def spilt(path,trainpath,testpath):
+    data = pd.read_csv(path,sep=',',index_col=0)
+    data1 = data.sample(frac=0.7,replace=False)#flase是无放回的切割，有放回的切
 #print(data1)
-data1.to_csv('../aapd/train.csv',',')
+    data1.to_csv(trainpath,',')
 
 
-data2 = data.sample(frac=0.3,replace=False)#flase是无放回的切割，有放回的切
+    data2 = data.sample(frac=0.3,replace=False)#flase是无放回的切割，有放回的切
 #print(data2)
-data2.to_csv('../aapd/test.csv',',')
+    data2.to_csv(testpath,',')
+
+spilt('../aapd/datacsv.csv','../aapd/train.csv','../aapd/test.csv')
